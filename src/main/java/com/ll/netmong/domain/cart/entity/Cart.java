ec902469da9eb19e4601be03ebaf8236e530e265
shinjaewon99 <tlswodnjs99@naver.com>
2024-01-15 11:27:29 +0900

    Refactor : 사용하지 않는 Index 제거

--- src/main/java/com/ll/netmong/domain/cart/entity/Cart.java
+++ src/main/java/com/ll/netmong/domain/cart/entity/Cart.java
@@ -10,9 +10,7 @@ import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
-@Table(name = "product_cart", indexes = {
-        @Index(name = "idx_member_id", columnList = "member_id")
-})
+@Table(name = "product_cart")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder(toBuilder = true)
 public class Cart extends BaseEntity {
